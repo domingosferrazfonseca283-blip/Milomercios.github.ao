eb68916c9267b01631b09e839f1c1faa07407fa6
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-08 14:24:56 +0100

    Adicionar Firebase Storage para comprovativos de pagamento

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwEcPZIAkJ8joGHjMqeNXMtf7bx3YxAvs",
+  apiKey: "AIzaSyBwEcPZIAKJ8joGHjMqeNXMtf7bx3YxAvs",
   authDomain: "milomercios-12106.firebaseapp.com",
   projectId: "milomercios-12106",
   storageBucket: "milomercios-12106.firebasestorage.app",
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
